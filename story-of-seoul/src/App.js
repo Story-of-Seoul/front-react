1d920809b899a82a4c28da1275b82213da340bc4
yongCloude <76439014+yongCloude@users.noreply.github.com>
2022-08-09 23:09:33 +0900

    [feat] 웹사이트 타이틀 및 아이콘 변경

--- story-of-seoul/src/App.js
+++ story-of-seoul/src/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
 import Router from './router/Router';
+import { Helmet } from 'react-helmet';
 
 const App = () => {
   return (
     <div className="App">
+      <div className='AppTitle'>
+        <Helmet>
+          <title>서울의 이야기</title>
+        </Helmet>
+      </div>
       <Nav/>
       <Router/>
     </div>
