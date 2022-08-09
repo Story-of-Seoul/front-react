f7ff0b48a21ca2ad113a269a460a62bcab8d8ab3
yongCloude <76439014+yongCloude@users.noreply.github.com>
2022-08-09 20:22:19 +0900

    [page] 웹 사이트 구성 페이지

---
+++ story-of-seoul/src/pages/DataAnalysis.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const DataAnalysis = () => {
+    return (
+        <div className='DataAnalysis'>
+            Data Analysis Page
+        </div>
+    );
+};
+
+export default DataAnalysis;
---
+++ story-of-seoul/src/pages/Participation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Participation = () => {
+    return (
+        <div className='Participation'>
+            Citizen Participation Page
+        </div>
+    );
+};
+
+export default Participation;
