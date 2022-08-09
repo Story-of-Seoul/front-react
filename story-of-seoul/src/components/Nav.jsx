39a0c52d49dbbc08b189bee19b3bb9e3c17b465a
yongCloude <76439014+yongCloude@users.noreply.github.com>
2022-08-09 20:22:30 +0900

    [ui] 네비게이션 바

---
+++ story-of-seoul/src/components/Nav.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+
+import '../styles/Nav.scss';
+import banner from '../images/banner.png';
+import { useNavigate } from 'react-router-dom';
+
+const Nav = () => {
+
+    const [isLogin, setIsLogin] = useState(false);
+
+    let navigate = useNavigate();
+    
+    const onClickMyPage = () => {
+        isLogin ? navigate("/mypage") : navigate("/signin");
+    }
+
+    return (
+        <div className="Nav">
+            <div className="ImgaeWrapper">
+                <img src={banner} alt="이미지 불러오기 오류" onClick={() => navigate("/")}/>
+            </div>
+            <div className="PageSelectorWrapper">
+                <div id="notice" onClick={() => navigate("/notice")}>공지사항</div>
+                <div id="analysis" onClick={() => navigate("/dataAnalysis")}>분석페이지</div>
+                <div id="citizen-participation" onClick={() => navigate("/participation")}>시민참여게시판</div>
+                <div id="data-request" onClick={() => navigate("/dataRequest")}>데이터요청</div>
+                <div id="my-page" onClick={() => onClickMyPage()}>마이페이지</div>
+            </div>
+        </div>
+    );
+};
+
+export default Nav;
