e22dcb49efd7b0db11913d24967a73834c8c5656
yongCloude <76439014+yongCloude@users.noreply.github.com>
2022-08-24 00:42:41 +0900

    [config] redux-dev-tool 설정

--- story-of-seoul/src/index.js
+++ story-of-seoul/src/index.js
@@ -9,7 +9,8 @@ import rootReducer from "./modules";
 import {Provider} from "react-redux";
 
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
